1cf94c260e3c83e876e97a6bca16ebab2c81c867
adefemi171 <adefemi171@gmail.com>
2019-12-04 20:27:45 -0500

    Added templates/polls/index.html and detail.html, edited poll/views.py and poll/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -9,6 +9,17 @@ from . import views
 # kwargs is an arbitrary keyword arguments and can be passed in a dictionary to the target view
 # name which allows naming your url which lets you refer to it unambigously from elsewhere in DjangoTemplates
 # name example here is: "name='index'"
+
+# added an app_name so Django can differentiates the URL names between several Django projects
+app_name = 'polls'
 urlpatterns = [
+    # /polls/
     path("", views.index, name='index'),
+    # /polls/<num>
+    # removed the word specifics
+    path('<str:question_id>/', views.detail, name='detail'),
+    # /polls/<num>/results
+    path('<str:question_id>/results/', views.results, name='results'),
+    # /polls/<num>/vote
+    path('<str:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+# from django.template import loader
+from django.http import Http404
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the index page")
+    latest_question_list = Question.objects.order_by('-pub_date') [:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # The render() function takes the request object as ut first argument: request.
+    # a template name as its second argument: 'polls/index.html'
+    # a dictionary as its optional third argument: context
+    # And it returns an HttpResponse object
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+# A URLconf maps URL pattern to views
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # The get_object_or_404 function takes a Django model as its first element: Question
+    # and and arbitrary number of keyword arguments wto the get() function of the model manager
+    # it then raises an Http404 if the object doesn't exist
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question} )
+
+def results(request, question_id):
+    response = "You're looking at the result of the vote of %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting for %s." %question_id)
